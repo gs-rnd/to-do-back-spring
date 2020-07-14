80efca727a2a4cd9dbc76c5ca3154ce7f4e2b95b
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-13 20:53:37 -0500

    initial commit

---
+++ src/main/java/io/github/gerritsmith/todobackspring/ToDoBackSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.gerritsmith.todobackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoBackSpringApplication.class, args);
+	}
+
+}
