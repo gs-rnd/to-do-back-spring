ccbc0b4add15e938b2c9558401033f7aa76bda6d
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-13 23:43:45 -0500

    added validation to new task requests

--- src/main/java/io/github/gerritsmith/todobackspring/task/TaskController.java
+++ src/main/java/io/github/gerritsmith/todobackspring/task/TaskController.java
@@ -1,10 +1,13 @@
 package io.github.gerritsmith.todobackspring.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -14,7 +17,7 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping("/tasks")
-    public Task addNewTask(@RequestBody Task task) {
+    public Task addNewTask(@RequestBody @Valid Task task) {
         return taskService.addTask(task);
     }
 
--- src/main/java/io/github/gerritsmith/todobackspring/task/TaskService.java
+++ src/main/java/io/github/gerritsmith/todobackspring/task/TaskService.java
@@ -2,6 +2,7 @@ package io.github.gerritsmith.todobackspring.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TaskService {
@@ -9,6 +10,7 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
+    @Transactional
     public Task addTask(Task task) {
         return taskRepository.save(task);
     }
